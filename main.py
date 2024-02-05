5d6af27847007813f433a7e477a9688ae0c6c4e7
Alex <licazago@fiec.espol.edu.ec>
2024-02-04 19:28:17 -0500

    comando con rutas relativas

--- main.py
+++ main.py
@@ -1,49 +1,26 @@
-import subprocess
 import os
-
-#ruta_magenta_music = os.path.join(os.path.dirname(__file__), 'magentaMusic')
-
-
-# Añade la ruta de magentaMusic al PATH
-#os.environ["PATH"] += os.pathsep + ruta_magenta_music
-
-# Define las rutas relativas dentro del proyecto
-#bundle_file = os.path.join(ruta_magenta_music, 'attention_rnn.mag')
-#output_dir = os.path.join(os.path.dirname(__file__), 'magentaMusic', 'output')
-#primer_midi = os.path.join(os.path.dirname(__file__), 'magentaMusic', 'archivo.mid')
-
-#comando = [
-#    "melody_rnn_generate",
- #   "--config=attention_rnn",
-  #  f"--bundle_file={bundle_file}",
-   # f"--output_dir={output_dir}",
-    #"--num_outputs=10",
-    #"--num_steps=128",
-    #f"--primer_midi={primer_midi}"
-#]
-
-#try:
- #   subprocess.run(comando, check=True)
-  #  print("Generación de melodías completada exitosamente.")
-#except subprocess.CalledProcessError as e:
- #   print(f"Error al generar melodías: {e}")
-
+import subprocess
 
 # Ruta al directorio que contiene melody_rnn_generate
-ruta_magenta_music = r"C:\Users\Migi\PreTaws\proyectoPreTaws\magentaMusic"
+ruta_magenta_music = os.path.join(os.path.dirname(__file__), 'magentaMusic')
 
 # Agregar la ruta al directorio a la variable de entorno PATH
 os.environ["PATH"] += os.pathsep + ruta_magenta_music
 
-# Define la ruta al ejecutable y los argumentos del comando
+# Define las rutas relativas dentro del proyecto
+bundle_file = os.path.join(ruta_magenta_music, 'attention_rnn.mag')
+output_dir = os.path.join(ruta_magenta_music, 'output')
+primer_midi = os.path.join(ruta_magenta_music, 'archivo.mid')
+
+# Define el comando usando rutas relativas
 comando = [
     "melody_rnn_generate",
     "--config=attention_rnn",
-    "--bundle_file=C:/Users/Migi/PreTaws/proyectoPreTaws/magentaMusic/attention_rnn.mag",
-    "--output_dir=C:/Users/Migi/PreTaws/proyectoPreTaws/magentaMusic/output",
+    f"--bundle_file={bundle_file}",
+    f"--output_dir={output_dir}",
     "--num_outputs=10",
     "--num_steps=128",
-    "--primer_midi=C:/Users/Migi/PreTaws/proyectoPreTaws/magentaMusic/archivo.mid"
+    f"--primer_midi={primer_midi}"
 ]
 
 try:
